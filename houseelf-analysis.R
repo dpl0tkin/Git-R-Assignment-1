2404ba136c87f415aa8bc73bba3e43d174159d25
David <dplotkin@ufl.edu>
2015-10-27 12:27:58 -0400

    Renamed data file and modified R script accordingly

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -2,3 +2,6 @@
 
 #This is an analysis of the relationship between the length of house-elves' 
 #ears and aspects of their DNA
+
+Elf = read.csv("data/houseelf_earlength_dna_data_1.csv", header = TRUE)
+
