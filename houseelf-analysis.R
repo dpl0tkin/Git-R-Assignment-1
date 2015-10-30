9effeaa7006cae721c3e4c53b3396bf0b2380a26
David <dplotkin@ufl.edu>
2015-10-30 12:26:02 -0400

    Add function to calculate GC context, regardless of letter case in the DNA sequence.

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -5,3 +5,15 @@
 
 Elf = read.csv("data/houseelf_earlength_dna_data_1.csv", header = TRUE)
 
+#Problem 6
+
+library(stringr)
+
+#Calculate GC content
+gc_content <- function(sequence){
+  dnaseq <- str_to_upper(sequence)
+  Gs <- str_count(dnaseq, 'G')
+  Cs <- str_count(dnaseq, 'C')
+  gc <- (Gs + Cs) / str_length(dnaseq) * 100
+  return(gc)
+} 
